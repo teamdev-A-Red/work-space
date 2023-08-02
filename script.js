83d8e956b06d367ce37f8ff6f5c1e7b8ce6b16e7
Atsuki Kitada <canvanvam@gmail.com>
2023-08-01 19:20:15 -0700

    draw game#4

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const h1 = document.querySelector(".h1");
-h1.innerHTML = "New Heading";
+const blockSize = 30;
+const boardRow = 20;
+const boardCol = 10;
+const cvs = document.getElementById("game");
+const ctx = cvs.getContext("2d");
+const canvasW = blockSize * boardCol;
+const canvasH = blockSize * boardRow;
+cvs.width = canvasW;
+cvs.height = canvasH;
+cvs.style.width = canvasW + "px";
+
+const draw = () => {
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, canvasW, canvasH);
+};
+//初期化処理
+const init = () => {
+  draw();
+};
