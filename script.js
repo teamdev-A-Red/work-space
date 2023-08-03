d259e62acbd5e578466891ca4ed4bc442c6a7a77
Atsuki Kitada <canvanvam@gmail.com>
2023-08-03 11:01:24 -0700

    コードのリファクタリングとブロックの移動

--- script.js
+++ script.js
@@ -45,63 +45,80 @@ class TetoriminoBoard {
     this.blockSize = 30;
     this.canvasW = this.blockSize * this.boardCol;
     this.canvasH = this.blockSize * this.boardRow;
+    this.setupCanvas();
+    this.setupInitialPosition();
+  }
+
+  setupCanvas() {
     this.cvs.width = this.canvasW;
     this.cvs.height = this.canvasH;
     this.cvs.style.width = this.canvasW + "px";
-    this.board = this.createEmptyArea();
-  };
+    this.ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+  }
 
-  createEmptyArea() {
-    this.gameArea = Array.from({ length: this.boardRow }, () =>
-      Array(this.boardCol).fill(0)
-    );
-    return this.gameArea;
-  };
+  setupInitialPosition() {
+    this.x = 4;
+    this.y = 0;
+  }
 
-  drawGameArea() {
-    for (let row = 0; row < this.gameArea.length; row++) {
-      for (let col = 0; col < this.gameArea[row].length; col++) {
-        if (this.gameArea[row][col] === 0) {
-          const blockX = col * this.blockSize;
-          const blockY = row * this.blockSize;
-          this.ctx.fillStyle = "rgba(0, 0, 0, 0)";
-          this.ctx.fillRect(blockX, blockY, this.blockSize, this.blockSize);
-        }
-      }
-    }
-  };
-
-  gameStart() {
-    this.ctx.clearRect(0, 0, this.cvs.width, this.cvs.height);
-
-    const startX = 4; // ブロックの開始位置X
-    const startY = 0; // ブロックの開始位置Y
+  drawBlock(shape, color) {
+    this.clearCanvas();
     const blockSize = this.blockSize;
 
-    // テトリスブロックの描画
-    this.ctx.strokeStyle = "rgba(0, 0, 0, 1)"; // 枠線の色を黒に設定
-
-    // テトリスブロックの形状を取得
-    const blockShape = SHAPES["L"]; // 例として "L" 形状を指定
-
-    for (let row = 0; row < blockShape.length; row++) {
-      for (let col = 0; col < blockShape[row].length; col++) {
-        if (blockShape[row][col] === 1) {
-          const x = (col + startX) * blockSize;
-          const y = (row + startY) * blockSize;
-          this.ctx.fillStyle = COLORS[1]; // ブロックの色を取得 (ここでは "L" ブロックは赤色に設定)
-          this.ctx.fillRect(x, y, blockSize, blockSize); // ブロックを塗りつぶす
-          this.ctx.strokeRect(x, y, blockSize, blockSize); // ブロックの枠線を描画
+    for (let row = 0; row < shape.length; row++) {
+      for (let col = 0; col < shape[row].length; col++) {
+        if (shape[row][col] === 1) {
+          const x = (col + this.x) * blockSize;
+          const y = (row + this.y) * blockSize;
+          this.drawSquare(x, y, blockSize, color);
         }
       }
     }
   }
-};
 
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.cvs.width, this.cvs.height);
+  }
 
+  drawSquare(x, y, size, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x, y, size, size);
+    this.ctx.strokeRect(x, y, size, size);
+  }
 
+  rotateBlock() {
+    // Implement the rotation logic here
+  }
 
+  moveRight() {
+    this.x++;
+  }
 
+  moveDown() {
+    this.y++;
+  }
+
+  moveLeft() {
+    this.x--;
+  }
+
+  handleKeyPress(event) {
+    switch (event.keyCode) {
+      case 37: // Left Arrow
+        this.moveLeft();
+        break;
+      case 38: // Up Arrow
+        this.rotateBlock();
+        break;
+      case 39: // Right Arrow
+        this.moveRight();
+        break;
+      case 40: // Down Arrow
+        this.moveDown();
+        break;
+    }
+  }
+}
 
 class GameBoard {
   constructor() {
@@ -138,12 +155,12 @@ class GameBoard {
     }
   }
 
-  placeBlock(block, color) {
+  placeBlock(block, color, x, y) {
     for (let row = 0; row < block.shape.length; row++) {
       for (let col = 0; col < block.shape[row].length; col++) {
         if (block.shape[row][col] === 1) {
-          const blockX = (this.x + col) * this.blockSize;
-          const blockY = (this.y + row) * this.blockSize;
+          const blockX = (x + col) * this.blockSize;
+          const blockY = (y + row) * this.blockSize;
           this.ctx.fillStyle = color;
           this.ctx.fillRect(blockX, blockY, this.blockSize, this.blockSize);
         }
@@ -152,54 +169,29 @@ class GameBoard {
   }
 }
 
-class Tetromino {
-  constructor(shapeName) {
-    this.shape = shapeName;
-    this.color = COLORS[Math.floor(Math.random() * 4) + 1];
-    this.x = Math.floor(this.boardCol / 2) - Math.floor(this.shape.length / 2);
-    this.y = 0;
-  }
+// const startGame = () => {
+//   // テトリスブロックを描画
+//   const tetoriminoBoard = new TetoriminoBoard(); // 新たなゲーム画面を作成
 
-  // moveUp() {
-  //   this.y++;
-  // }
-
-  // moveRight() {
-  //   this.x++;
-  // }
-
-  // moveDown() {
-  //   this.y++;
-  // }
-
-  // moveLeft() {
-  //   this.x--;
-  // }
-}
-
-const initializeGame = () => {
-  const newGame = new GameBoard();
-  newGame.drawGameArea();
-  const newBlock = new Tetromino(SHAPES.L);
-  console.log(newBlock);
-  newGame.placeBlock(newBlock, newBlock.color);
-};
-
-const init = () => {
-  initializeGame();
-};
-
-// document.addEventListener("keydown", (event) => {
-//   if (event.key === "ArrowLeft") {
-//     newBlock.moveLeft();
-//   } else if (event.key === "ArrowRight") {
-//     newBlock.moveRight();
-//   } else if (event.key === "ArrowDown") {
-//     newBlock.moveDown();
-//   }
-// });
+//   return newBlock;
+// };
 
 const startGame = () => {
-  const tetoriminoBoard = new TetoriminoBoard(); // 新たなゲーム画面を作成
-  tetoriminoBoard.gameStart(); // テトリスブロックを描画
+  const newGame = new GameBoard();
+  newGame.drawGameArea();
+  const tetoriminoBoard = new TetoriminoBoard();
+  const newBlock = tetoriminoBoard.drawBlock(SHAPES.L, COLORS[1]);
+  console.log(newBlock);
+  window.addEventListener("keydown", (event) => {
+    tetoriminoBoard.handleKeyPress(event);
+  });
+
+  function gameLoop() {
+    tetoriminoBoard.drawBlock(SHAPES.L, COLORS[1]);
+    requestAnimationFrame(gameLoop);
+  }
+
+  gameLoop();
 };
+
+// ページが読み込まれた際にテトリスゲームを初期化
